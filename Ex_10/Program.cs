b6d99568f10c5dd6d4957bd1a1083c3f31bbd7e0
Igor_Demchenko <idemch777@gmail.com>
2022-10-08 08:45:59 +0300

    Add Ex_10

---
+++ Ex_10/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Программа, которая принимает 
+//  на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int EnterNumber()
+{
+    int number;
+    while (true)
+    {
+        Console.Write("Введите трехзначное число: ");
+        int.TryParse(Console.ReadLine(), out number);
+        if (99 < number && number < 1000) break;
+        else Console.WriteLine("Ошибка ввода!");
+    }
+    return number;
+}
+
+int num = EnterNumber();
+int a1 = num % 100;
+int a2 = a1 / 10;
+Console.WriteLine($"Число {num} -> {a2}");
