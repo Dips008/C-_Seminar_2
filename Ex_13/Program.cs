b714b4b8a0326caad0d5226dd46489bbac05950c
Igor_Demchenko <idemch777@gmail.com>
2022-10-08 09:33:49 +0300

    Add Ex_13

---
+++ Ex_13/Program.cs
@@ -0,0 +1,30 @@
+﻿// Программа, которая выводит третью 
+// цифру заданного числа (включая отрицательные) или сообщает, что третьей цифры нет.
+
+int EnterNumber()
+{
+    int number;
+    while (true)
+    {
+        Console.Write("Введите число: ");
+        if (int.TryParse(Console.ReadLine(), out number)) break;
+        else Console.WriteLine("Ошибка ввода!");
+    }
+    return number;
+}
+int num = EnterNumber();
+int num1 = Math.Abs(num);
+int result = num1;
+if (num1 < 100)
+{
+    Console.WriteLine($"{num} ->  третьей цифры нет");
+}
+else
+{
+    while (result > 1000)
+    {
+        result = result / 10;
+    }
+    result = result % 10;
+    Console.WriteLine($"{num} ->{result}");
+}
